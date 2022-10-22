3521abee7ac9420f3bea7c80bde81bf30a32c4c9
math0898 <25396616+math0898@users.noreply.github.com>
2022-10-22 10:48:23 -0500

    Started Game Launcher - Created a basic game launcher. - Added AI generated poster art for Pong and LOZ.

--- src/main/java/io/github/math0898/main/Main.java
+++ src/main/java/io/github/math0898/main/Main.java
@@ -8,22 +8,25 @@ import suga.engine.input.keyboard.StackGameKeyListener;
 import suga.engine.input.mouse.BasicMouseListener;
 import suga.engine.logger.Level;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.Objects;
 
 /**
- * Main execution class for testing the Graphics Library.
+ * Main execution class for the Game Suite.
  *
  * @author Sugaku
  */
-public class Main extends GameEngine {
+public class Main {
 
     /**
-     * Runs the testing program for the graphics engine.
+     * Runs a game included in the sweet depending on arguments.
      *
-     * @param args The arguments given to the java program.
+     * @param args The arguments given to the game.
      */
-    public static void main (String[] args) {
+    public static void startGame (String[] args) {
         GameEngine.getLogger().setLevel(Level.DEBUG);
         GraphicsPanel panel = new Graphics2d();
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
@@ -34,5 +37,45 @@ public class Main extends GameEngine {
         GameEngine.launchGameWindow(size.width, size.height, "SugaEngine - " + target, false,
                 panel, Color.BLACK, 60, 60, new StackGameKeyListener(), new BasicMouseListener(), game);
         game.loadScene(Objects.equals(target, "PONG") ? "Main Menu" : "Title Screen"); // todo: Do games need a start method to call the first scene?
+    }
+
+    /**
+     * Main execution point for Game Suite. Starts a basic game launcher to display options.
+     *
+     * @param args The arguments given to the java program.
+     */
+    public static void main (String[] args) throws IOException {
+        GameEngine.getLogger().log("Launcher: Starting game launcher...");
+        JFrame frame = new JFrame();
+        frame.setSize(1024, 547);
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("Game Suite Launcher");
+        JButton loz = new JButton("", new ImageIcon(ImageIO.read(Main.class.getResource("/loz/Poster.png"))));
+        loz.setBorder(BorderFactory.createEmptyBorder());
+        loz.setContentAreaFilled(true);
+        loz.addActionListener((e) -> {
+            startGame(new String[]{ "LOZ" });
+            GameEngine.getLogger().log("Launcher: Hiding game launcher.");
+            frame.setVisible(false);
+        });
+        JButton pong = new JButton("" , new ImageIcon(ImageIO.read(Main.class.getResource("/pong/Poster.png"))));
+        pong.addActionListener((e) -> {
+            startGame(new String[]{ "PONG" });
+            GameEngine.getLogger().log("Launcher: Hiding game launcher.");
+            frame.setVisible(false);
+        });
+        pong.setBorder(BorderFactory.createEmptyBorder());
+        pong.setContentAreaFilled(true);
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        frame.add(loz, c);
+        c.gridx = 1;
+        frame.add(pong, c);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(Color.BLACK);
+        frame.setUndecorated(false);
+        frame.setVisible(true);
+        GameEngine.getLogger().log("Launcher: Launcher started.");
     }
 }
